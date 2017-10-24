4ef39efaddd2456d076c0193076c72166dec3120
Alex Proca <alex.proca@gmail.com>
2017-10-25 00:37:11 +0300

    Added jdbc realm

--- sakila-business-webapi/src/main/java/isep/web/sakila/webapi/config/WebSecurityConfig.java
+++ sakila-business-webapi/src/main/java/isep/web/sakila/webapi/config/WebSecurityConfig.java
@@ -4,14 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        // No Auth
+//        http.authorizeRequests().anyRequest().permitAll();
+
+        // Auth
         http
             .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
@@ -26,9 +32,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    public void configureMemoryRealm(AuthenticationManagerBuilder auth) throws Exception {
+
         auth
-            .inMemoryAuthentication()
-                .withUser("zpeng").password("zpeng").roles("USER");
+                .inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("USER");
+
+
     }
+
+//    @Autowired
+//    public void configureJDBCRealm(AuthenticationManagerBuilder auth, DataSource dataSource) throws Exception {
+//
+//        auth
+//                .jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery(
+//                        "select username,password, enabled from users where username=?")
+//                .authoritiesByUsernameQuery(
+//                        "select username, role from user_roles where username=?")
+//        ;
+//
+//    }
 }
