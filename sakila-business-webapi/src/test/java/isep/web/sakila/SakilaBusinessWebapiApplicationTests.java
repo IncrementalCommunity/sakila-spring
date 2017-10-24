cf182401fd3569596dc98423002eff3141779714
Alex Proca <alex.proca@gmail.com>
2017-10-25 00:13:42 +0300

    Updated project

--- sakila-business-webapi/src/test/java/isep/web/sakila/SakilaBusinessWebapiApplicationTests.java
+++ sakila-business-webapi/src/test/java/isep/web/sakila/SakilaBusinessWebapiApplicationTests.java
@@ -1,14 +1,13 @@
 package isep.web.sakila;
 
+import isep.web.sakila.webapi.SakilaBusinessWebapiApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import isep.web.sakila.webapi.SakilaBusinessWebapiApplication;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SakilaBusinessWebapiApplication.class)
+@SpringBootTest(classes = SakilaBusinessWebapiApplication.class)
 public class SakilaBusinessWebapiApplicationTests {
 
 	@Test
--- sakila-business-webapi/src/test/java/isep/web/sakila/webapi/service/ActorServiceTest.java
+++ sakila-business-webapi/src/test/java/isep/web/sakila/webapi/service/ActorServiceTest.java
@@ -1,17 +1,16 @@
 package isep.web.sakila.webapi.service;
 
+import isep.web.sakila.jpa.config.PersistenceConfig;
+import isep.web.sakila.webapi.model.ActorWO;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import isep.web.sakila.jpa.config.PersistenceConfig;
-import isep.web.sakila.webapi.model.ActorWO;
 
-
-@SpringApplicationConfiguration(classes = PersistenceConfig.class)
+@SpringBootTest(classes = PersistenceConfig.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ActorServiceTest {
 
